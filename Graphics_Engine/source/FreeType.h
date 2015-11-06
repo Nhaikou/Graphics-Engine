63f3479cc8f2bf94da47e918ce8e2ff50fa801e7
Hakerai <elrik9303@gmail.com>
2015-11-06 12:52:34 +0200

    Freetype new try

--- Graphics_Engine/source/FreeType.h
+++ Graphics_Engine/source/FreeType.h
@@ -1,40 +1,39 @@
 //////Laatija: Juuso Martikainen
 //
-//#ifndef FREE_TYPE_H
-//#define FREE_TYPE_H
-////FreeType
+#ifndef FREE_TYPE_H
+#define FREE_TYPE_H
+//FreeType
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <freetype\ftglyph.h>
+#include <freetype\ftoutln.h>
+#include <freetype\fttrigon.h>
+
+//openGl
+#include <Windows.h>
+#include <GL\GL.h>
+#include <GL\GLU.h>
+
+#include <vector>
+#include <string>
+
+
+#include <stdexcept>
+
+class fontData
+{
+	float Height;
+	GLuint * textures;
+	GLuint listBase;
+public:
+
+//	Init functio luo fontin korkeudella Height tiedostosta fname.
 //
-//#include <ft2build.h>
-//#include FT_FREETYPE_H
-//#include <freetype\freetype.h>
-//#include <freetype\ftglyph.h>
-//#include <freetype\ftoutln.h>
-//#include <freetype\fttrigon.h>
-//
-////openGl
-//#include <Windows.h>
-//#include <GL\GL.h>
-//#include <GL\GLU.h>
-//
-//#include <vector>
-//#include <string>
-//
-//
-//#include <stdexcept>
-//
-//class fontData
-//{
-//	float Height;
-//	GLuint * textures;
-//	GLuint listBase;
-//public:
-//
-//	
-//
-//	void init(const char *fname, unsigned int Height);
-//
-//	void clean();
-//
-//	void print(const fontData &ftFont, float x, float y, const char *fmt, ...);
-//};
-//#endif
+	void init(const char *fname, unsigned int Height);
+//Vapauttaa kaikki resurssit jotka ovat fonteissa
+	void clean();
+
+	void print(const fontData &ftFont, float x, float y, const char *fmt, ...);
+};
+#endif
